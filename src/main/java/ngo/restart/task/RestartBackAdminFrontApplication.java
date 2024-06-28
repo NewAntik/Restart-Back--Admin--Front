3c32b079f0281b91cfb0c81021e612b3ba45f0cb
Antik <120013201+NewAntik@users.noreply.github.com>
2024-06-28 14:41:02 +0300

    first commit

---
+++ src/main/java/ngo/restart/task/RestartBackAdminFrontApplication.java
@@ -0,0 +1,12 @@
+package ngo.restart.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestartBackAdminFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestartBackAdminFrontApplication.class, args);
+	}
+}
---
+++ src/test/java/ngo/restart/task/RestartBackAdminFrontApplicationTests.java
@@ -0,0 +1,12 @@
+package ngo.restart.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestartBackAdminFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
